bed0481cf93a24d8d13b88adb69f80a873410c40
f1nalspace <finalspace@googlemail.com>
2023-03-28 13:08:31 +0200

    Fixed Platform was not implementing equatable properly and operators

--- CPU8086/Platform.cs
+++ CPU8086/Platform.cs
@@ -5,16 +5,17 @@ namespace Final.CPU8086
     public enum PlatformType
     {
         None = 0,
-        _32Bit,
+        _8087,
         _186,
         _286,
         _386,
         _387,
+        _32Bit,
         _486,
-        Pentium,
+        _586,
     }
 
-    public readonly struct Platform
+    public readonly struct Platform : IEquatable<Platform>
     {
         public PlatformType Type { get; }
 
@@ -23,12 +24,23 @@ namespace Final.CPU8086
             Type = type;
         }
 
+        public bool Equals(Platform other) => Type == other.Type;
+        public override int GetHashCode() => Type.GetHashCode();
+        public override bool Equals(object obj) => obj is Platform platform && Equals(platform);
+
+        public static bool operator ==(Platform left, Platform right) => left.Equals(right);
+        public static bool operator !=(Platform left, Platform right) => !(left == right);
+        public static bool operator <(Platform left, Platform right) => left.Type < right.Type;
+        public static bool operator >(Platform left, Platform right) => left.Type > right.Type;
+
         public static Platform Parse(string value)
         {
             if (string.IsNullOrWhiteSpace(value))
                 return new Platform();
             switch (value.ToLower())
             {
+                case "8087":
+                    return new Platform(PlatformType._8087);
                 case "186":
                     return new Platform(PlatformType._186);
                 case "286":
@@ -42,12 +54,14 @@ namespace Final.CPU8086
                 case "32bit":
                     return new Platform(PlatformType._32Bit);
                 case "p5":
-                    return new Platform(PlatformType.Pentium);
+                    return new Platform(PlatformType._586);
                 default:
                     throw new NotSupportedException($"The platform '{value}' is not supported!");
             }
         }
 
         public override string ToString() => Type.ToString();
+
+        
     }
 }

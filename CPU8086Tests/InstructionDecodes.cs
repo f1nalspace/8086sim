8293d9bacb9ea67e274f856b3a3c3d68d2b25eb4
f1nalspace <finalspace@googlemail.com>
2023-04-02 11:03:48 +0200

    Added more length 2 unit-tests

--- CPU8086Tests/InstructionDecodes.cs
+++ CPU8086Tests/InstructionDecodes.cs
@@ -2,6 +2,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 
@@ -58,30 +59,107 @@ namespace Final.CPU8086
         public void TestLength2To4Decode()
         {
             CPU cpu = new CPU();
-            Assert.AreEqual<Instruction>(cpu.DecodeNext(new byte[] { 0x00, 0b11011000 }, "ADD AL, BL"), new Instruction(0x00, 2, InstructionType.ADD, DataWidthType.Byte, new InstructionOperand(RegisterType.AL), new InstructionOperand(RegisterType.BL)));
-            Assert.AreEqual<Instruction>(cpu.DecodeNext(new byte[] { 0x02, 0b11000011 }, "ADD BL, AL"), new Instruction(0x02, 2, InstructionType.ADD, DataWidthType.Byte, new InstructionOperand(RegisterType.BL), new InstructionOperand(RegisterType.AL)));
+            Assert.AreEqual<Instruction>(new Instruction(0x00, 2, InstructionType.ADD, DataWidthType.Byte, new InstructionOperand(RegisterType.AL), new InstructionOperand(RegisterType.BL)), cpu.DecodeNext(new byte[] { 0x00, 0b11011000 }, "ADD AL, BL"));
+            Assert.AreEqual<Instruction>(new Instruction(0x02, 2, InstructionType.ADD, DataWidthType.Byte, new InstructionOperand(RegisterType.BL), new InstructionOperand(RegisterType.AL)), cpu.DecodeNext(new byte[] { 0x02, 0b11000011 }, "ADD BL, AL"));
+
+            Assert.AreEqual("DAA", cpu.DecodeNext(new byte[] { 0x27 }, "DAA").GetAssembly());
+            Assert.AreEqual("PUSH ES", cpu.DecodeNext(new byte[] { 0x06 }, "PUSH es").GetAssembly());
         }
 
         [TestMethod]
         public void TestLength1Decode()
         {
             CPU cpu = new CPU();
-            Assert.AreEqual<Instruction>(cpu.DecodeNext(new byte[] { 0x27 }, "DAA"), new Instruction(0x27, 1, InstructionType.DAA, DataWidthType.None));
-            Assert.AreEqual<Instruction>(cpu.DecodeNext(new byte[] { 0x06 }, "PUSH es"), new Instruction(0x06, 1, InstructionType.PUSH, DataWidthType.None, new InstructionOperand(RegisterType.ES)));
+
+            Assert.AreEqual<Instruction>(
+                new Instruction(0x27, 1, InstructionType.DAA, DataWidthType.None), 
+                cpu.DecodeNext(new byte[] { 0x27 }, "DAA"));
+            Assert.AreEqual<Instruction>(
+                new Instruction(0x06, 1, InstructionType.PUSH, DataWidthType.None, new InstructionOperand(RegisterType.ES)), 
+                cpu.DecodeNext(new byte[] { 0x06 }, "PUSH es"));
+
+            Assert.AreEqual("DAA", cpu.DecodeNext(new byte[] { 0x27 }, "DAA").GetAssembly());
+            Assert.AreEqual("PUSH ES", cpu.DecodeNext(new byte[] { 0x06 }, "PUSH es").GetAssembly());
         }
 
         [TestMethod]
         public void TestLength2Decode()
         {
             CPU cpu = new CPU();
-            Assert.AreEqual<Instruction>(cpu.DecodeNext(new byte[] { 0x04, 42 }, "ADD"), new Instruction(0x04, 2, InstructionType.ADD, DataWidthType.Byte, new InstructionOperand(RegisterType.AL), new InstructionOperand(42, ImmediateFlag.None)));
+
+            Span<byte> mov_CX_BX = new byte[] { 0x89, 0xD9 };
+            Assert.AreEqual<Instruction>(
+                new Instruction(mov_CX_BX[0], 2, InstructionType.MOV, DataWidthType.Word, new InstructionOperand(RegisterType.CX), new InstructionOperand(RegisterType.BX)),
+                cpu.DecodeNext(mov_CX_BX, "MOV CX, BX"));
+            Span<byte> mov_CH_AH = new byte[] { 0x88, 0xE5 };
+            Assert.AreEqual<Instruction>(
+                new Instruction(mov_CH_AH[0], 2, InstructionType.MOV, DataWidthType.Byte, new InstructionOperand(RegisterType.CH), new InstructionOperand(RegisterType.AH)),
+                cpu.DecodeNext(mov_CH_AH, "MOV CH, AH"));
+            Span<byte> mov_DX_BX = new byte[] { 0x89, 0xDA };
+            Assert.AreEqual<Instruction>(
+                new Instruction(mov_DX_BX[0], 2, InstructionType.MOV, DataWidthType.Word, new InstructionOperand(RegisterType.DX), new InstructionOperand(RegisterType.BX)),
+                cpu.DecodeNext(mov_DX_BX, "MOV DX, BX"));
+            Span<byte> mov_SI_BX = new byte[] { 0x89, 0xDE };
+            Assert.AreEqual<Instruction>(
+                new Instruction(mov_SI_BX[0], 2, InstructionType.MOV, DataWidthType.Word, new InstructionOperand(RegisterType.SI), new InstructionOperand(RegisterType.BX)),
+                cpu.DecodeNext(mov_SI_BX, "MOV SI, BX"));
+            Span<byte> mov_BX_DI = new byte[] { 0x89, 0xFB };
+            Assert.AreEqual<Instruction>(
+                new Instruction(mov_BX_DI[0], 2, InstructionType.MOV, DataWidthType.Word, new InstructionOperand(RegisterType.BX), new InstructionOperand(RegisterType.DI)),
+                cpu.DecodeNext(mov_BX_DI, "MOV BX, DI"));
+            Span<byte> mov_AL_CL = new byte[] { 0x88, 0xC8 };
+            Assert.AreEqual<Instruction>(
+                new Instruction(mov_AL_CL[0], 2, InstructionType.MOV, DataWidthType.Byte, new InstructionOperand(RegisterType.AL), new InstructionOperand(RegisterType.CL)),
+                cpu.DecodeNext(mov_AL_CL, "MOV AL, CL"));
+            Span<byte> mov_CH_CH = new byte[] { 0x88, 0xED };
+            Assert.AreEqual<Instruction>(
+                new Instruction(mov_CH_CH[0], 2, InstructionType.MOV, DataWidthType.Byte, new InstructionOperand(RegisterType.CH), new InstructionOperand(RegisterType.CH)),
+                cpu.DecodeNext(mov_CH_CH, "MOV CH, CH"));
+            Span<byte> mov_BX_AX = new byte[] { 0x89, 0xC3 };
+            Assert.AreEqual<Instruction>(
+                new Instruction(mov_BX_AX[0], 2, InstructionType.MOV, DataWidthType.Word, new InstructionOperand(RegisterType.BX), new InstructionOperand(RegisterType.AX)),
+                cpu.DecodeNext(mov_BX_AX, "MOV BX, AX"));
+            Span<byte> mov_BX_SI = new byte[] { 0x89, 0xF3 };
+            Assert.AreEqual<Instruction>(
+                new Instruction(mov_BX_SI[0], 2, InstructionType.MOV, DataWidthType.Word, new InstructionOperand(RegisterType.BX), new InstructionOperand(RegisterType.SI)),
+                cpu.DecodeNext(mov_BX_SI, "MOV BX, SI"));
+            Span<byte> mov_SP_DI = new byte[] { 0x89, 0xFC };
+            Assert.AreEqual<Instruction>(
+                new Instruction(mov_SP_DI[0], 2, InstructionType.MOV, DataWidthType.Word, new InstructionOperand(RegisterType.SP), new InstructionOperand(RegisterType.DI)),
+                cpu.DecodeNext(mov_SP_DI, "MOV SP, DI"));
+            Span<byte> mov_BP_AX = new byte[] { 0x89, 0xC5 };
+            Assert.AreEqual<Instruction>(
+                new Instruction(mov_BP_AX[0], 2, InstructionType.MOV, DataWidthType.Word, new InstructionOperand(RegisterType.BP), new InstructionOperand(RegisterType.AX)),
+                cpu.DecodeNext(mov_BP_AX, "MOV BP, AX"));
+
+            Span<byte> add_AL_0x2A = new byte[] { 0x04, 0x2A };
+            Assert.AreEqual<Instruction>(
+                new Instruction(add_AL_0x2A[0], 2, InstructionType.ADD, DataWidthType.Byte, new InstructionOperand(RegisterType.AL), new InstructionOperand(42, ImmediateFlag.None)), 
+                cpu.DecodeNext(add_AL_0x2A, "ADD AL, 0x2A"));
+
+            Assert.AreEqual("MOV CX, BX", cpu.DecodeNext(mov_CX_BX, "MOV CX, BX").GetAssembly());
+            Assert.AreEqual("MOV CH, AH", cpu.DecodeNext(mov_CH_AH, "MOV CH, AH").GetAssembly());
+            Assert.AreEqual("MOV DX, BX", cpu.DecodeNext(mov_DX_BX, "MOV DX, BX").GetAssembly());
+            Assert.AreEqual("MOV SI, BX", cpu.DecodeNext(mov_SI_BX, "MOV SI, BX").GetAssembly());
+            Assert.AreEqual("MOV BX, DI", cpu.DecodeNext(mov_BX_DI, "MOV BX, DI").GetAssembly());
+            Assert.AreEqual("MOV AL, CL", cpu.DecodeNext(mov_AL_CL, "MOV AL, CL").GetAssembly());
+            Assert.AreEqual("MOV CH, CH", cpu.DecodeNext(mov_CH_CH, "MOV CH, CH").GetAssembly());
+            Assert.AreEqual("MOV BX, SI", cpu.DecodeNext(mov_BX_SI, "MOV BX, SI").GetAssembly());
+            Assert.AreEqual("MOV BX, AX", cpu.DecodeNext(mov_BX_AX, "MOV BX, AX").GetAssembly());
+            Assert.AreEqual("MOV SP, DI", cpu.DecodeNext(mov_SP_DI, "MOV SP, DI").GetAssembly());
+            Assert.AreEqual("MOV BP, AX", cpu.DecodeNext(mov_BP_AX, "MOV BP, AX").GetAssembly());
+
+            Assert.AreEqual("ADD AL, 0x2A", cpu.DecodeNext(add_AL_0x2A, "ADD AL 0x2A").GetAssembly());
         }
 
         [TestMethod]
         public void TestLength3Decode()
         {
             CPU cpu = new CPU();
-            Assert.AreEqual<Instruction>(cpu.DecodeNext(new byte[] { 0x05, 0x03, 0xF0 }, "ADD"), new Instruction(0x05, 3, InstructionType.ADD, DataWidthType.Word, new InstructionOperand(RegisterType.AX), new InstructionOperand((short)-4093, ImmediateFlag.None)));
+
+            Assert.AreEqual<Instruction>(
+                new Instruction(0x05, 3, InstructionType.ADD, DataWidthType.Word, new InstructionOperand(RegisterType.AX), new InstructionOperand((short)-4093, ImmediateFlag.None)),
+                cpu.DecodeNext(new byte[] { 0x05, 0x03, 0xF0 }, "ADD"));
         }
 
         [TestMethod]

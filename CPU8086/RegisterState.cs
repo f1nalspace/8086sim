84dacd5fb8e8b25d5802b3c47e045a24e733b45f
f1nalspace <finalspace@googlemail.com>
2023-04-19 13:45:53 +0200

    Properly handle property changed for register state

--- CPU8086/RegisterState.cs
+++ CPU8086/RegisterState.cs
@@ -1,90 +1,103 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 namespace Final.CPU8086
 {
-    [StructLayout(LayoutKind.Explicit, Pack = 1)]
-    public class RegisterState
+    public class RegisterState : INotifyPropertyChanged
     {
-        [FieldOffset(0)]
-        private short _AX;
-        [FieldOffset(0)]
-        private sbyte _AL;
-        [FieldOffset(1)]
-        private sbyte _AH;
+        public event PropertyChangedEventHandler PropertyChanged;
 
-        public short AX { get => _AX; set => _AX = value; }
-        public sbyte AL { get => _AL; set => _AL = value; }
-        public sbyte AH { get => _AH; set => _AH = value; }
+        private void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 
-        [FieldOffset(2)]
-        private short _BX;
-        [FieldOffset(2)]
-        private sbyte _BL;
-        [FieldOffset(3)]
-        private sbyte _BH;
+        private void RaisePropertiesChanged(params string[] propertyNames)
+        {
+            foreach (string propertyName in propertyNames)
+                RaisePropertyChanged(propertyName);
+        }
 
-        public short BX { get => _BX; set => _BX = value; }
-        public sbyte BL { get => _BL; set => _BL = value; }
-        public sbyte BH { get => _BH; set => _BH = value; }
+        private void SetValue<T>(ref T reference, T value, Action callback = null, [CallerMemberName] string propertyName = null)
+        {
+            T oldValue = reference;
+            if (object.Equals(oldValue, value))
+                return;
+            reference = value;
+            RaisePropertyChanged(propertyName);
+            if (callback != null)
+                callback.Invoke();
+        }
 
-        [FieldOffset(4)]
-        private short _CX;
-        [FieldOffset(4)]
-        private sbyte _CL;
-        [FieldOffset(5)]
-        private sbyte _CH;
+        [StructLayout(LayoutKind.Explicit, Pack = 1)]
+        struct R16
+        {
+            [FieldOffset(0)]
+            public short X;
+            [FieldOffset(0)]
+            public sbyte L;
+            [FieldOffset(1)]
+            public sbyte H;
+        }
 
-        public short CX { get => _CX; set => _CX = value; }
-        public sbyte CL { get => _CL; set => _CL = value; }
-        public sbyte CH { get => _CH; set => _CH = value; }
+        private R16 _ax = new R16();
+        public short AX { get => _ax.X; set => SetValue(ref _ax.X, value, () => RaisePropertiesChanged(nameof(AL), nameof(AH))); }
+        public sbyte AL { get => _ax.L; set => SetValue(ref _ax.L, value, () => RaisePropertyChanged(nameof(AX))); }
+        public sbyte AH { get => _ax.H; set => SetValue(ref _ax.H, value, () => RaisePropertyChanged(nameof(AX))); }
 
-        [FieldOffset(6)]
-        private short _DX;
-        [FieldOffset(6)]
-        private sbyte _DL;
-        [FieldOffset(7)]
-        private sbyte _DH;
+        private R16 _bx = new R16();
+        public short BX { get => _bx.X; set => SetValue(ref _bx.X, value, () => RaisePropertiesChanged(nameof(BL), nameof(BH))); }
+        public sbyte BL { get => _bx.L; set => SetValue(ref _bx.L, value, () => RaisePropertyChanged(nameof(BX))); }
+        public sbyte BH { get => _bx.H; set => SetValue(ref _bx.H, value, () => RaisePropertyChanged(nameof(BX))); }
 
-        public short DX { get => _DX; set => _DX = value; }
-        public sbyte DL { get => _DL; set => _DL = value; }
-        public sbyte DH { get => _DH; set => _DH = value; }
+        private R16 _cx = new R16();
+        public short CX { get => _cx.X; set => SetValue(ref _cx.X, value, () => RaisePropertiesChanged(nameof(CL), nameof(CH))); }
+        public sbyte CL { get => _cx.L; set => SetValue(ref _cx.L, value, () => RaisePropertyChanged(nameof(CX))); }
+        public sbyte CH { get => _cx.H; set => SetValue(ref _cx.H, value, () => RaisePropertyChanged(nameof(CX))); }
 
-        [FieldOffset(8)]
-        private short _SP;
-        [FieldOffset(10)]
-        private short _BP;
-        [FieldOffset(12)]
-        private short _SI;
-        [FieldOffset(14)]
-        private short _DI;
+        private R16 _dx = new R16();
+        public short DX { get => _dx.X; set => SetValue(ref _dx.X, value, () => RaisePropertiesChanged(nameof(DL), nameof(DH))); }
+        public sbyte DL { get => _dx.L; set => SetValue(ref _dx.L, value, () => RaisePropertyChanged(nameof(DX))); }
+        public sbyte DH { get => _dx.H; set => SetValue(ref _dx.H, value, () => RaisePropertyChanged(nameof(DX))); }
 
-        public short SP { get => _SP; set => _SP = value; }
-        public short BP { get => _BP; set => _BP = value; }
-        public short SI { get => _SI; set => _SI = value; }
-        public short DI { get => _DI; set => _DI = value; }
+        private short _SP = 0;
+        private short _BP = 0;
+        private short _SI = 0;
+        private short _DI = 0;
+        public short SP { get => _SP; set => SetValue(ref _SP, value); }
+        public short BP { get => _BP; set => SetValue(ref _BP, value); }
+        public short SI { get => _SI; set => SetValue(ref _SI, value); }
+        public short DI { get => _DI; set => SetValue(ref _DI, value); }
 
-        [FieldOffset(16)]
-        private short _CS;
-        [FieldOffset(18)]
-        private short _DS;
-        [FieldOffset(20)]
-        private short _SS;
-        [FieldOffset(22)]
-        private short _ES;
+        private short _CS = 0;
+        private short _DS = 0;
+        private short _SS = 0;
+        private short _ES = 0;
+        public short CS { get => _CS; set => SetValue(ref _CS, value); }
+        public short DS { get => _DS; set => SetValue(ref _DS, value); }
+        public short SS { get => _SS; set => SetValue(ref _SS, value); }
+        public short ES { get => _ES; set => SetValue(ref _ES, value); }
 
-        public short CS { get => _CS; set => _CS = value; }
-        public short DS { get => _DS; set => _DS = value; }
-        public short SS { get => _SS; set => _SS = value; }
-        public short ES { get => _ES; set => _ES = value; }
+        private ushort _IP = 0;
+        public ushort IP { get => _IP; set => SetValue(ref _IP, value); }
 
-        [FieldOffset(24)]
-        private ushort _IP;
-        public ushort IP { get => _IP; set => _IP = value; }
+        private ushort _Status = 0;
+        public ushort Status { get => _Status; set => SetValue(ref _Status, value); }
 
-        [FieldOffset(26)]
-        private ushort _Status;
-        public ushort Status { get => _Status; set => _Status = value; }
+        public const byte ZeroFlagBit = 6;
+        public const ushort ZeroFlagMask = 1 << ZeroFlagBit;
+
+        public bool ZeroFlag
+        {
+            get => (Status & ZeroFlagMask) == ZeroFlagMask;
+            set
+            {
+                if (value)
+                    Status = (ushort)(Status & ~ZeroFlagMask);
+                else
+                    Status = (ushort)(Status | ZeroFlagMask);
+            } 
+        }
 
         public void Reset()
         {

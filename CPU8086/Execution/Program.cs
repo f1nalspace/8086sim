08d5f47dccbec77963fd3d0de7499753847ca6d2
Finalspace <finalspace@gmail.com>
2024-05-01 08:35:52 +0200

    Halt back scan for programs

--- CPU8086/Execution/Program.cs
+++ CPU8086/Execution/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Immutable;
+﻿using System;
+using System.Collections.Immutable;
 using System.IO;
 
 namespace Final.CPU8086.Execution
@@ -10,20 +11,50 @@ namespace Final.CPU8086.Execution
         public RegisterState Register { get; }
         public int Length => Stream.Length;
 
-        public Program(string name, ImmutableArray<byte> stream, RegisterState register = default)
+        public Program(string name, ReadOnlySpan<byte> stream, RegisterState register = default)
         {
             Name = name;
-            Stream = stream;
+
+            int haltPos = BackScanForHalt(stream);
+
+            if (haltPos == -1)
+                Stream = stream.ToImmutableArray();
+            else
+                Stream = stream.Slice(0, haltPos + 1).ToImmutableArray();
+
             Register = register;
         }
 
         public Program(string name, Stream stream, RegisterState register = default)
         {
             Name = name;
+
             byte[] data = new byte[stream.Length];
             stream.Read(data, 0, data.Length);
-            Stream = data.ToImmutableArray();
+
+            int haltPos = BackScanForHalt(data);
+
+            if (haltPos == -1)
+                Stream = data.ToImmutableArray();
+            else
+                Stream = data.AsSpan(0, haltPos + 1).ToImmutableArray();
+
             Register = register;
+        }
+
+        public static int BackScanForHalt(ReadOnlySpan<byte> data)
+        {
+            for (int index = data.Length - 1; index > 0; index--)
+            {
+                byte opCode = data[index];
+                if (opCode == 0xF4 && index < data.Length - 2)
+                {
+                    byte nextByte = data[index + 1];
+                    if (nextByte == 0)
+                        return index;
+                }
+            }
+            return -1;
         }
 
         public override string ToString() => Name;
